3973527b591dd48d362489312143b311f3e07cac
JzzzHung <jzhung1013@gmail.com>
2025-03-05 18:52:00 +0800

    v2024.1

--- main.py
+++ main.py
@@ -2,41 +2,35 @@ from urllib.request import urlretrieve
 from zipfile import ZipFile
 import os
 import requests
-from bs4 import BeautifulSoup
 
 VERSION_TXT = 'version.txt'
 DST_PATH = './driver'
 DRIVER_ZIP = f'{DST_PATH}/win.zip'
 DRIVER_TMP = 'chromedriver-win64/chromedriver.exe'
 DRIVER_EXE = f'{DST_PATH}/chromedriver.exe'
-DRIVER_ABS_PATH_TXT = f'{DST_PATH}/path.txt'
-CHROME_INDEX = 'https://googlechromelabs.github.io/chrome-for-testing'
+CHROME_QUERY = 'https://googlechromelabs.github.io/chrome-for-testing/LATEST_RELEASE_'
+CHROMEDRIVER_URL = 'https://storage.googleapis.com/chrome-for-testing-public/{}/win64/chromedriver-win64.zip'
 
 # make driver dir
 if not os.path.isdir(DST_PATH):
     print('create a folder.')
     os.mkdir(DST_PATH)
 
-# get driver version
+# get chrome version
 with open(VERSION_TXT, encoding='utf-16') as file:
     local_ver = file.read().strip().split('=')[-1]
     main_ver = local_ver.split('.')[0]
-print('main ver: ', main_ver) # 87
-print('ver: ', local_ver)     # 87.0.4280.141
 
-# match ver in Chrome-Driver-Page
-print('driver is updating...')
-response = requests.get(CHROME_INDEX)
-soup = BeautifulSoup(response.text, 'lxml')
-ver = soup.find('code').string    # '87.0.4280.88'
+# get version of Chrome-Driver
+ver = requests.get(CHROME_QUERY + main_ver).text    # '87.0.4280.88'
+print('chrome ver: ', main_ver) # 87
+print('driver ver: ', ver)     # 87.0.4280.141
 
 # download driver zip
-print('driver is updating..')
-url = f'https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/{ver}/win64/chromedriver-win64.zip'
+url = CHROMEDRIVER_URL.format(ver)
 urlretrieve(url, DRIVER_ZIP)
 
 # delete old driver.exe
-print('driver is updating.')
 if os.path.isfile(DRIVER_EXE):
     os.remove(DRIVER_EXE)
 
